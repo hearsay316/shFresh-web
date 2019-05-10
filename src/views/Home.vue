80d18a642d71df7e6ccfd7482f65415da97b52c6
hearsay123 <qazwsx2228@163.com>
2019-05-10 17:44:13 +0800

    添加了eleui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button @click="login">按钮</button>
+    <button @click="getuser">按钮</button>
   </div>
 </template>
 
@@ -11,6 +12,42 @@ import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'home',
+  mounted(){
+axios.get("http://localhost:8080/register").then((res)=>{
+  console.log(res);
+}).catch((error)=>{
+  console.log(error);
+})
+  },
+  methods:{
+    login(){
+      let obj = {
+        userName:"swswd",
+        password:"swdswsw"
+      };
+      axios({
+        method: 'post',
+        withCredentials:true,
+        url: 'http://localhost:8080/register',
+        data: {
+          username:"swswd",
+          password:"swdswsw"
+        }
+      });
+    },getuser(){
+      //("http://localhost:8080/Article/user")
+      axios({
+        method: 'get',
+        withCredentials:true,
+        url: 'http://localhost:8080/Article/user',
+      }).then((res)=>{
+        console.log(res);
+      }).catch((error)=>{
+        console.log(error);
+      })
+    }
+
+  },
   components: {
     HelloWorld
   }
